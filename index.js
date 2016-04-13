451123a9f807b17cf15d91c169f43fe158e5194c
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-13 08:20:39 -0400

    Added main index.html

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express     = require("express");
 var app         = express();
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(process.env.PORT || 3001, function(){

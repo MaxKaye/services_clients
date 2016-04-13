18267bf00e89238eab4507d3feda3d74791b6f54
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-13 08:27:11 -0400

    Added Angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log("Hello")
+"use strict";
+
+(function(){
+  angular
+  .module("myApp", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config(Router);
+
+  Router.$inject = ["$stateProvider"];
+  function Router($stateProvider){
+    $stateProvider
+    .state("main", {
+      url:      "/",
+      template: "This is working"
+    });
+  }
+})();
